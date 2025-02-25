e0ba984941c0164fe7154643f05c2ec95d669326
hkhb <h14hbk@gmail.com>
2025-02-25 09:51:49 +0900

    返り値をuserに変更

--- src/backend/modules/auth/auth.controller.ts
+++ src/backend/modules/auth/auth.controller.ts
@@ -1,20 +1,21 @@
 import { Controller, Post, Body} from "@nestjs/common";
 import { AuthRepository } from "./auth.repository";
 import { LoginDto } from "../shared/login.dto";
+import { User } from "@prisma/client/default";
 
 @Controller('auth')
 export class AuthController {
   constructor(private AuthRepository: AuthRepository) {}
 
   @Post('login')
-  async login(@Body() loginFormData: LoginDto): Promise<object> {
+  async login(@Body() loginFormData: LoginDto): Promise<User | null> {
     const existUser = await this.AuthRepository.checkExistUser(loginFormData);
     return existUser;
   }
   
-  @Post('changeInitialPassword')
-    async changeInitialPassword(@Body() changeInitialPasswordFormData: changeInitialPasswordDto): Promise<boolean>{
-      const isChangeInitialPassword = await this.AuthRepository.changeInitialPassword(changeInitialPasswordFormData);
-      return isChangeInitialPassword
-    }
-  }
+  // @Post('changeInitialPassword')
+  // async changeInitialPassword(@Body() changeInitialPasswordFormData: changeInitialPasswordDto): Promise<boolean>{
+  //   const isChangeInitialPassword = await this.AuthRepository.changeInitialPassword(changeInitialPasswordFormData);
+  //   return isChangeInitialPassword
+  // }
+}
--- src/backend/modules/auth/auth.repository.ts
+++ src/backend/modules/auth/auth.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service'
 import { LoginDto } from '../shared/login.dto';
+import { User } from '@prisma/client/default';
 // import * as jwt from 'jsonwebtoken';
 
 @Injectable()
@@ -12,20 +13,21 @@ export class AuthRepository {
   // 存在している場合は、true
   //　違う場合は、false
   
-  // async checkExistUser(loginFormData: LoginDto): Promise<object> {
-  //   const user =  await this.prisma.user.findFirst({
-  //     where: {email: loginFormData.email, password: loginFormData.password}
-  //   });
-  //   let token: string | null = null;
-  //   if(user){
-  //     token = jwt.sign(
-  //       { userId: user.id, admin: user.isAdmin, isitialPassword: user.isInitinalPassword },  // ペイロード部分に必要な情報を含める
-  //     process.env.JWT_SECRET,                // 環境変数からシークレットキーを読み込む
-  //     { expiresIn: '1h' } 
-  //     )
-  //   }
-  //   return {status: user !== null,
-  //           token: token
-  //   };
-  // }
+  async checkExistUser(loginFormData: LoginDto): Promise<User | null> {
+    const user =  await this.prisma.user.findFirst({
+      where: {email: loginFormData.email, password: loginFormData.password}
+    });
+    // let token: string | null = null;
+    // if(user){
+    //   token = jwt.sign(
+    //     { userId: user.id, admin: user.isAdmin, isitialPassword: user.isInitinalPassword },  // ペイロード部分に必要な情報を含める
+    //   process.env.JWT_SECRET,                // 環境変数からシークレットキーを読み込む
+    //   { expiresIn: '1h' } 
+    //   )
+    // }
+    // return {status: user !== null,
+    //         token: token
+    // };
+    return user;
+  }
 }

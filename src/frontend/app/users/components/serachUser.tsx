e9bf0233346c40b782700f236901326d7523dc4f
hkhb <h14hbk@gmail.com>
2025-02-27 10:07:45 +0900

    ボタンの非活性化

--- src/frontend/app/users/components/serachUser.tsx
+++ src/frontend/app/users/components/serachUser.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { Box, Input, Button} from "@chakra-ui/react";
 import { User } from '../page';
+import React, { useEffect, useState } from 'react';
 
 type SearchUserProps = {
   setUser: React.Dispatch<React.SetStateAction<User[]>>;
@@ -18,15 +18,20 @@ const serachProp = {
 // 挙動:
 //   入力された文字列をbuttonが押された場合、テーブルに結果を反映。
 //   文字列がなかった場合は検索ボタンは非活性
-const SearchUser: React.FC<SearchUserProps> = ({ setUser }: SearchUserProps) => {  
+const SearchUser: React.FC<SearchUserProps> = ({ setUser }: SearchUserProps) => {
+
+  const [filterName, setFilterName] = useState<string>("");
+
+  console.log(filterName);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setFilterName(e.target.value);
+  const handleButtonClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {console.log(e)}
+
   return (
-    <>
-      <Box {...serachProp}>
-        <Input {...marginProps}/>
-        <Button {...marginProps}>検索</Button>
-      </Box>
-    </>
-    
+    <Box {...serachProp}>
+      <Input onChange={handleInputChange} {...marginProps}/>
+      <Button isDisabled={filterName === ""} onClick={handleButtonClick} {...marginProps}>検索</Button>
+    </Box>
   );
 };
 
